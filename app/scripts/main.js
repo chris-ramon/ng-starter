a2f74244d617ca1463356a48d77bae5587126dc4
Chris Ramón <rchristian.ramon@gmail.com>
2014-08-24 15:28:13 -0500

    simple services using .service, .factory .provider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,26 @@
 
 var demoApp = angular.module('demoApp', []);
 
-demoApp.controller('MainCtrl', function() {
-
+demoApp.provider('FooService', function() {
+  this.$get = function() {
+    return {foo: this.foo};
+  };
 });
+
+demoApp.config(function(FooServiceProvider) {
+  FooServiceProvider.foo = 'foo service';
+});
+
+demoApp.controller('MainCtrl', function($injector) {
+  //debugger;
+});
+
+demoApp.factory('BarService', function() {
+  return {bar: 'bar service'};
+});
+
+demoApp.service('BazService', function() {
+  this.baz = 'baz service';
+});
+
+
